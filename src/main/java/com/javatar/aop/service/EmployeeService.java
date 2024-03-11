c07668a5f8de304e7e66c98ecaa45f4a2209c285
AbdiJavad <Mirjavadabdi@gmail.com>
2024-03-11 21:41:37 +0330

    Updated,Changed

--- src/main/java/com/javatar/aop/service/EmployeeService.java
+++ src/main/java/com/javatar/aop/service/EmployeeService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class EmployeeService {
+
     public Employee createEmployee(Employee employee) {
         Employee emp = new Employee();
         emp.setEmpId(employee.getEmpId());
