e3fc983d1239c7e668ecbe3600079daf5be3db0a
AbdiJavad <Mirjavadabdi@gmail.com>
2024-03-12 23:21:19 +0330

    Updated,Changed

--- src/main/java/com/javatar/aop/controller/EmployeeController.java
+++ src/main/java/com/javatar/aop/controller/EmployeeController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("employee")
 public class EmployeeController {
-
     @Autowired
     private EmployeeService employeeService;
 
